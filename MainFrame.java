7322b62d3ed309a839e1462bd952dfcc6f84f7c0
kenton <159168431+debussy1@users.noreply.github.com>
2024-04-07 18:46:10 -0500

    Update MainFrame.java

--- MainFrame.java
+++ MainFrame.java
@@ -1,32 +1,54 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.awt.FlowLayout;
 
 public class MainFrame {
-
     public static void main(String[] args) {
-        // title for new frame
         JFrame frame = new JFrame("Group name");
-
-        // set layout
         frame.setLayout(new FlowLayout());
 
-        // adds buttons and assigns names
-        JButton button1 = new JButton("Grant");
-        JButton button2 = new JButton("Bryan");
-        JButton button3 = new JButton("Jacob");
-        JButton button4 = new JButton("Ali");
-        JButton button5 = new JButton("Kenton");
+        // creating each member's class
+        frame.add(new GrantClass());
+        frame.add(new BryanClass());
+        frame.add(new JacobClass());
+        frame.add(new AliClass());
+        frame.add(new KentonClass());
 
-        frame.add(button1);
-        frame.add(button2);
-        frame.add(button3);
-        frame.add(button4);
-        frame.add(button5);
-
-        // properties of frame
+        // setting up the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 200);
         frame.setVisible(true);
+    }
+}
+
+// each member's individual class
+class GrantClass extends JPanel {
+    public GrantClass() {
+        add(new JButton("Grant"));
+    }
+}
+
+class BryanClass extends JPanel {
+    public BryanClass() {
+        add(new JButton("Bryan"));
+    }
+}
+
+class JacobClass extends JPanel {
+    public JacobClass() {
+        add(new JButton("Jacob"));
+    }
+}
+
+class AliClass extends JPanel {
+    public AliClass() {
+        add(new JButton("Ali"));
+    }
+}
+
+class KentonClass extends JPanel {
+    public KentonClass() {
+        add(new JButton("Kenton"));
     }
 }
